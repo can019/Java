273bbd9b3b9ce68e42cdbaaaf01e7e3a4b9385ad
can019 <jys01012@gmail.com>
2021-01-18 22:31:29 +0900

    error occured when add tree

--- 11725/src/Main.java
+++ 11725/src/Main.java
@@ -36,11 +36,15 @@ public class Main {
         private Node root;
         private int size;
 
-        public Tree(int value) {
-            Node new_Node = new Node(value);
-            this.root = new_Node;
+        public Tree() {
             this.size = 0;
+            this.root = null;
         }
+        public void set_root(Node root){
+            this.root = root;
+            this.size_up();
+        }
+
         private void size_up(){
             this.size++;
         }
@@ -55,11 +59,14 @@ public class Main {
         public Node get_root(){
             return this.root;
         }
-        public boolean add(int parent_value, Node new_node){
+        public boolean add(int parent_value, int value){
+            Node new_node = new Node(value);
             Queue<Node> in_sequence = new LinkedList<>();
             in_sequence.add(this.get_root());
             Node parent = this.find_bfs(in_sequence, parent_value);
 
+            if(parent == null)
+                return false;
             if (parent.get_left()==null)
                 parent.set_left(new_node);
             else if(parent.get_right()==null)
@@ -71,15 +78,21 @@ public class Main {
             return true;
         }
         public Node find_bfs(Queue<Node> in_sequence, int target_value){
-            Node current = in_sequence.poll();
-            if (current.get_value()==target_value)
-                return current;
-            else{
-                if(current.get_left()!=null)
-                    in_sequence.add(current.get_left());
-                if(current.get_right()!=null)
-                    in_sequence.add(current.get_right());
-                return find_bfs(in_sequence, target_value);
+            try {
+                Node current = in_sequence.poll();
+                if (current.get_value() == target_value)
+                    return current;
+                else {
+                    if (current.get_left() != null)
+                        in_sequence.add(current.get_left());
+                    if (current.get_right() != null)
+                        in_sequence.add(current.get_right());
+                    return find_bfs(in_sequence, target_value);
+                }
+            }
+            catch (Exception e){
+                System.out.println();
+                return null;
             }
         }
         public void print_parent_node(int target_value){
@@ -90,14 +103,43 @@ public class Main {
         }
     }
 
+    public static boolean make_tree(Tree tree, HashMap<Integer, Stack<Integer>> map, Stack<Integer> courses){
+        int current_target = courses.pop();
+        System.out.println(current_target);
+        if(current_target==1){
+            Node new_node = new Node(current_target);
+            tree.set_root(new_node);
+        }
+        Stack<Integer> stack = map.get(current_target);
+        if(current_target==2)
+            System.out.println();
+        if(stack != null){
+            while(!stack.isEmpty()){
+                int value = stack.pop();
+                courses.add(value);
+                tree.add(current_target, value);
+            }
+            map.remove(current_target);
+        }
+        if(map.size()==0){
+            return true;
+        }
+        else if(courses.size()==0){
+            Iterator<Integer> keys = map.keySet().iterator();
+            courses.add(keys.next());
+        }
+        return make_tree(tree, map, courses);
+    }
+
+
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
         int n_input = input.nextInt();
         HashMap<Integer, Stack<Integer>> map = new HashMap<>();
 
-        for(int i = 0; i < n_input; i++){
-            int key = input.nextInt();
-            int value = input.nextInt();
+        for(int i = 0; i < n_input-1; i++){
+            int key = Integer.parseInt(input.next());
+            int value = Integer.parseInt(input.next());
             if(!map.containsKey(key)){
                 Stack<Integer> stack = new Stack<>();
                 stack.push(value);
@@ -106,11 +148,26 @@ public class Main {
             else{
                 map.get(key).push(value);
             }
+            System.out.println(i);
         }
-
-        for (int key : map.keySet()) {
-            map.get(key);
-        }
-
+        Tree tree = new Tree();
+        System.out.println("--------------------------");
+       Stack<Integer> courses = new Stack<>();
+       courses.add(1);
+       make_tree(tree, map, courses);
+       System.out.println();
+//        for (int key : map.keySet()) {
+//            System.out.println(key);
+//            Stack<Integer> stack = new Stack<>();
+//            stack = map.get(key);
+//            if(key==1){
+//                Node new_node = new Node(key);
+//                tree.set_root(new_node);
+//            }
+//            while(!stack.isEmpty()){
+//                tree.add(key, stack.pop());
+//            }
+//        }
+//        System.out.println();
     }
 }
