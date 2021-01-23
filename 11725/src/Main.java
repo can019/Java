71a15a434144bf15bd17c7838bbf9792524a744e
can019 <jys01012@gmail.com>
2021-01-23 19:39:27 +0900

    Tree - Implemment :: Done

--- 11725/src/Main.java
+++ 11725/src/Main.java
@@ -36,13 +36,57 @@ public class Main {
         private Node root;
         private int size;
 
-        public Tree(int value){
+        public Tree(int value) {
             Node new_Node = new Node(value);
             this.root = new_Node;
             this.size = 0;
         }
-        public boolean add(Node new_node){
+        private void size_up(){
+            this.size++;
+        }
+        public int get_size(){
+            return this.size;
+        }
+        public boolean is_empty(){
+            if(this.get_size()==0)
+                return true;
+            return false;
+        }
+        public Node get_root(){
+            return this.root;
+        }
+        public boolean add(int parent_value, Node new_node){
+            Queue<Node> in_sequence = new LinkedList<>();
+            in_sequence.add(this.get_root());
+            Node parent = this.find_bfs(in_sequence, parent_value);
 
+            if (parent.get_left()==null)
+                parent.set_left(new_node);
+            else if(parent.get_right()==null)
+                parent.set_right(new_node);
+            else
+                return false;
+
+            this.size_up();
+            return true;
+        }
+        public Node find_bfs(Queue<Node> in_sequence, int target_value){
+            Node current = in_sequence.poll();
+            if (current.get_value()==target_value)
+                return current;
+            else{
+                if(current.get_left()!=null)
+                    in_sequence.add(current.get_left());
+                if(current.get_right()!=null)
+                    in_sequence.add(current.get_right());
+                return find_bfs(in_sequence, target_value);
+            }
+        }
+        public void print_parent_node(int target_value){
+            Queue<Node> in_sequence = new LinkedList<>();
+            in_sequence.add(this.get_root());
+            Node target = this.find_bfs(in_sequence, target_value);
+            System.out.println(target.get_parent().get_value());
         }
     }
 
